8adc956bed92bdfc661df860e5229e174e3831e9
Lucas Marinho <marinho.dev98@gmail.com>
2025-06-20 19:03:04 -0300

    Milestone 1

---
+++ grim-warden/src/main/java/com/grimwarden/grim_warden/GrimWardenApplication.java
@@ -0,0 +1,13 @@
+package com.grimwarden.grim_warden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrimWardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrimWardenApplication.class, args);
+	}
+
+}
---
+++ grim-warden/src/test/java/com/grimwarden/grim_warden/GrimWardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grimwarden.grim_warden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrimWardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
